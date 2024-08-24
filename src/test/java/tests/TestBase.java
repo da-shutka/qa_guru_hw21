257323583ac47d181733f82fec68489534555be2
Daria Petrova <38262231+da-shutka@users.noreply.github.com>
2024-08-24 04:19:01 +0300

    Removed sout

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,7 +21,6 @@ public class TestBase {
         if (System.getProperty("deviceHost") == null) {
             System.setProperty("deviceHost", "browserstack");
         }
-        System.out.println("deviceHost=" + System.getProperty("deviceHost"));
         switch (System.getProperty("deviceHost")) {
             case "browserstack":
                 Configuration.browser = BrowserStackDriver.class.getName();
